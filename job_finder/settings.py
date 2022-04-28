c8f6ab6a3944dcb12362987576e430944c719b2a
dhirajkafle553 <dhirajkafle553@gmail.com>
2022-04-28 23:28:58 +0545

    Register and Login functionality done

--- job_finder/settings.py
+++ job_finder/settings.py
@@ -164,8 +164,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True  
-EMAIL_HOST_USER = 'email_address'
-EMAIL_HOST_PASSWORD = 'password_of_email'
+EMAIL_HOST_USER = 'dhirajkafle998@gmail.com'
+EMAIL_HOST_PASSWORD = 'hitman47lastproject'
 
 
 
